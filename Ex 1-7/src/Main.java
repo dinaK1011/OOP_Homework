dae31aa772e2be47bef5f590eeb700d2425d6109
״Dina” <dina10112003@gmail.com>
2026-07-18 17:21:46 +0300

    Ex 2

--- Ex 1-7/src/Main.java
+++ Ex 1-7/src/Main.java
@@ -1,17 +1,30 @@
-public static void main() {
+import java.awt.*;
 
-//    Ex1
+public class Main {
+    static void main(String[] args) {
+        // Ex1
 
-//    public class SomethingIsWrong {
-//        public static void main(String[] args) {
-//            Rectangle myRect;
-//            myRect.width = 40;
-//            myRect.height = 50;
-//            System.out.println("myRect's area is " + myRect.area());
-//        }
-//    }
+        //    public class SomethingIsWrong {
+        //        public static void main(String[] args) {
+        //            Rectangle myRect;
+        //            myRect.width = 40;
+        //            myRect.height = 50;
+        //            System.out.println("myRect's area is " + myRect.area());
+        //        }
+        //    }
 
-    //The class Rectangle myRect is not created yet.
+        //The class Rectangle myRect is not created yet.
+
+        // Ex2
+        public class SomethingIsWrong {
+            public static void main(String[] args) {
+                Rectangle myRect = new Rectangle();
+                myRect.width = 40;
+                myRect.height = 50;
+                System.out.println("myRect's area is " + myRect.area());
+            }
+        }
 
 
+    }
 }
